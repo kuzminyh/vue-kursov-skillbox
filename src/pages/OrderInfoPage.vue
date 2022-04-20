6221bb906ad58d43c57f00914febd9fb355740ff
Кузьминых Сергей Александрович <Kuzminyh_sa@irkvkx.ru>
2022-04-20 14:58:50 +0800

    to home

--- src/pages/OrderInfoPage.vue
+++ src/pages/OrderInfoPage.vue
@@ -77,7 +77,7 @@ export default {
       return this.$store.state.orderInfo;
     },
     delivery() {
-      return this.$store.state.orderInfo.deliveryType.title;
+      return this.$store.state.orderInfo.deliveryType.id;
     },
   },
   created() {
